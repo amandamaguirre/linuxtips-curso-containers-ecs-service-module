06e4c641ff0c352e32205b93d6192947fecb9321
Amanda Aguirre <amandamorenoaguirre@gmail.com>
2024-10-21 18:22:56 -0300

    Nova pipeline

--- variables.tf
+++ variables.tf
@@ -2,6 +2,11 @@ variable "region" {}
 
 variable "service_name" {}
 
+variable "container_image" {
+  type = string
+  description = "Imagem com tag"
+}
+
 variable "cluster_name" {}
 
 variable "vpc_id" {}
